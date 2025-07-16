3c936e29d842f92ded8e3bda6d6b234af799f3ce
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-15 20:52:04 -0500

    Update setZeroes.py

--- setZeroes.py
+++ setZeroes.py
@@ -7,3 +7,16 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+
+        rowZero = False
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
