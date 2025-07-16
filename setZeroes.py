1fd9d548b9cc47e78962d116624f47290b95374f
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-15 20:51:39 -0500

    Update setZeroes.py

--- setZeroes.py
+++ setZeroes.py
@@ -1,2 +1,9 @@
 
 #73. Set Matrix Zeroes
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
